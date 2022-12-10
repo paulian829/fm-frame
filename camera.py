6273e413be978f44539c97273bf49925bf9da1d6
jeanian26 <paulian829@gmail.com>
2022-12-10 15:16:34 +0800

    update

--- camera.py
+++ camera.py
@@ -4,13 +4,23 @@ import numpy as np
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-
+from playsound import playsound
 
 class VideoCamera(object):
     def __init__(self):
         # Using OpenCV to capture from device 0.
-
+        print('test if initated succesfully')
         self.stream = WebcamVideoStream(src=0).start()
+        prototxtPath = "face_detector/deploy.prototxt"
+        weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+        self.faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+        self.soundOn = False
+        self.reset_sound = False
+        
+        print("[INFO] loading face mask detector model...")
+        filen = "mask_detector.model"
+        self.maskNet = load_model(filen)
+        print("Face mask model loaded")
 
     def __del__(self):
         self.stream.stop()
@@ -18,7 +28,6 @@ class VideoCamera(object):
     def predict(self, frame, faceNet, maskNet):
         # grab the dimensions of the frame and then construct a blob
         # from it
-        print("face detection started")
         (h, w) = frame.shape[:2]
         blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
                                      (104.0, 177.0, 123.0))
@@ -26,7 +35,6 @@ class VideoCamera(object):
         # pass the blob through the network and obtain the face detections
         faceNet.setInput(blob)
         detections = faceNet.forward()
-        print("face Detection done")
         # initialize our list of faces, their corresponding locations,
         # and the list of predictions from our face mask network
         faces = []
@@ -64,15 +72,14 @@ class VideoCamera(object):
                 # lists
                 faces.append(face)
                 locs.append((startX, startY, endX, endY))
-                print("mask predict started")
         # only make a predictions if at least one face was detected
         if len(faces) > 0:
+            print(len(faces))
             # for faster inference we'll make batch predictions on *all*
             # faces at the same time rather than one-by-one predictions
             # in the above `for` loop
             faces = np.array(faces, dtype="float32")
             preds = maskNet.predict(faces, batch_size=32)
-            print("mask predict done")
         # return a 2-tuple of the face locations and their corresponding
         # locations
         return (locs, preds)
@@ -81,24 +88,28 @@ class VideoCamera(object):
         image = self.stream.read()
 
         #load face detect model from disk
-        prototxtPath = r"C:\Users\asus\Desktop\PROJECT NETHRA\face_detector\deploy.prototxt"
-        weightsPath = r"C:\Users\asus\Desktop\PROJECT NETHRA\face_detector\res10_300x300_ssd_iter_140000.caffemodel"
-        faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+        # prototxtPath = "face_detector/deploy.prototxt"
+        # weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+        # faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
         # load the face mask detector model from disk
-        print("[INFO] loading face mask detector model...")
-        filen = r"C:\Users\asus\Desktop\PROJECT NETHRA\mask_detector.model"
-        maskNet = load_model(filen)
-        print("Face mask model loaded")
+        # print("[INFO] loading face mask detector model...")
+        # filen = "mask_detector.model"
+        # maskNet = load_model(filen)
+        # print("Face mask model loaded")
         while True:
             # detect faces in the frame and determine if they are wearing a
             # face mask or not
-
-            (locs, preds) = VideoCamera.predict(self, image, faceNet, maskNet)
-            print("function done")
+            try:
+                (locs, preds) = VideoCamera.predict(self, image, self.faceNet, self.maskNet)
+            except:
+                (locs, preds) = VideoCamera.predict(self, image, self.faceNet, self.maskNet)
             # loop over the detected face locations and their corresponding
             # locations
-
+            print(locs,preds)
+            if len(locs) == 0:
+                print('reset')
+                self.soundOn =False
             for (box, pred) in zip(locs, preds):
                 # unpack the bounding box and predictions
                 (startX, startY, endX, endY) = box
@@ -108,7 +119,17 @@ class VideoCamera(object):
                 # the bounding box and text
                 label = "Mask" if mask > withoutMask else "No Mask"
                 color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
+                print(label)
+                if label == 'Mask':
+                    self.soundOn = False
+                    
+                    
+                else:
+                    if self.soundOn == False:
+                        self.soundOn = True
+                        playsound('warning sound.mp3')
+                    
+                    
                 # include the probability in the label
                 label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
